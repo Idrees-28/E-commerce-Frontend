a0488cc20a85eb76c11465c9986b3e65ffa5069a
purushothaman-003 <003purushoth@gmail.com>
2024-09-25 00:03:37 -0700

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+        <div>
+            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <p className='w-full md:w-2/3 text-gray-600'>
+            At Forever, we believe that fashion is an expression of who you are. Founded in 1987, we set out to create a brand that offers stylish, high-quality clothing for everyone. Our team is passionate about design and dedicated to bringing you the latest trends at affordable prices.
+            </p>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>+91 9083837282</li>
+                <li>purushothaman17dev@gmail.com</li>
+            </ul>
+        </div>
+
+      </div>
+
+        <div>
+            <hr />
+            <p className='py-5 text-sm text-center'>Copyright 2024@ forever.com - All Right Reserved.</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
